885c5116de06e9a5c7c0ca4a65ff4d8ab816dff4
Qudus Lawal <lawalqudus980@gmail.com>
2023-10-18 10:25:07 +0100

    Change equality operator to strict equality operator

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const rockPaperScissor = function(playerSelection, computerSelection) {
     //check if player choice and comChoice is the same 
     if(playerSelection === computerSelection){
         return `It's a Tie!, `
-    } else if((playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissor' && computerSelection == 'Paper') || (playerSelection == 'rock ' && computerSelection == 'scissor')){ //check player choices against computer choices
+    } else if((playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissor' && computerSelection === 'Paper') || (playerSelection === 'rock ' && computerSelection === 'scissor')){ //check player choices against computer choices
         return `${playerSelection} VS ${computerSelection} : You Won!`
     } else { //check computer choices against player choices
         return `${computerSelection} VS ${playerSelection}: Computer Wins!`
@@ -28,10 +28,17 @@ const rockPaperScissor = function(playerSelection, computerSelection) {
 const game = function() {
     //create a loop to make 5 round of game
     for(let i = 0; i < 5; i++){
+        //Declare variable for player selection
         let playerSelection = prompt('Rock, Paper, or Scissor; Insert your choice here to begin playing').toLowerCase();
+        //Declare variable for computer selection
         let computerSelection = getComputerChoice();
+
+        //calling the rock paper scissor in the game function
+        console.log(rockPaperScissor(playerSelection,computerSelection));
     }
 }
+
+game();
 //
 // console.log(getComputerChoice())
 // let userInput = prompt("Rock, Paper, or Scissors ?, insert your choice here...").toLowerCase();
